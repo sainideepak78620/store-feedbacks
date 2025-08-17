d241b1f41c1afe55d2b58c241c8deb37ad72ea0e
sainideepak78620 <sainideepak78620@gmail.com>
2025-08-17 17:30:41 +0530

    Updated logic

--- backend/server.js
+++ backend/server.js
@@ -11,29 +11,25 @@ app.use(cors());
 app.use(express.json());
 
 // Load quotes
-// const quotes = require("./quotes.json");
+const quotes = require("./quotes.json");
 
 // GET /quote - return a random motivational quote
-// app.get("/quote", (req, res) => {
-//   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-//   res.json({ quote: randomQuote });
-// });
+app.get("/quote", (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json({ quote: randomQuote });
+});
 
 // POST /feedback - save feedback to feedback.json
 app.post("/feedback", (req, res) => {
-  const { name, email, feedback } = req.body;
-  console.log(name, email, feedback);
-  if (!name || !feedback) {
-    return res.status(400).json({ error: "Name and feedback are required" });
+  const { name, email, feedback } = req.body
+  if (!name || !email || !feedback) {
+    return res.status(400).json({ error: "Name, Email and feedback are required" });
   }
 
-  let feedbackData = [];
-  console.log("hello")
+  let feedbackData = []
   if (fs.existsSync(feedbackFilePath)) {
     const data = fs.readFileSync(feedbackFilePath, "utf8");
-    console.log(data)
     feedbackData = JSON.parse(data);
-    console.log(feedbackData)
   }
 
   feedbackData.push({ name, feedback, date: new Date().toISOString() });
@@ -43,19 +39,13 @@ app.post("/feedback", (req, res) => {
   res.json({ message: "Feedback submitted successfully!" });
 });
 
+// Get /feedbacks - return saved feedbacks
 app.get("/feedbacks", (req, res) => {
     let feedbackData = []
     if (fs.existsSync(feedbackFilePath)) {
         const data = fs.readFileSync(feedbackFilePath, "utf8");
-        console.log(data)
         feedbackData = JSON.parse(data);
-        console.log(feedbackData)
-      }
-    
-    //   feedbackData.push({ name, feedback, date: new Date().toISOString() });
-    
-    //   fs.writeFileSync(feedbackFilePath, JSON.stringify(feedbackData, null, 2));
-    
+      }    
       res.json(feedbackData);
 });
 
